4e34f207810622b26d10b56f8290a9d54b92d2bd
fmalmeida <felipemarques89@gmail.com>
2023-10-16 20:58:37 +0200

    start bug fix

--- build_conda.sh
+++ build_conda.sh
@@ -9,6 +9,7 @@ conda convert -p osx-64 $(find build -name "falmeida-py*.tar.bz2")
 
 # upload osx
 anaconda upload $(find osx-64 -name "falmeida-py*.tar.bz2") --force
+sleep 140
 anaconda upload $(find build/linux-64 -name "falmeida-py*.tar.bz2") --force
 
 # rm dirs
